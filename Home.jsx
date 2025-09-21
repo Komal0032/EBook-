6f98eeafacae4bed25c5c3ba94d9e27b264795e7
Komal Jadhav <158317145+Komal0032@users.noreply.github.com>
2025-09-21 11:50:52 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="container-fluid vh-100 d-flex justify-content align-items-center" style={{ backgroundImage: "url('https://media.istockphoto.com/id/671101136/photo/business-and-education-background.webp?a=1&b=1&s=612x612&w=0&k=20&c=kUP8MvUn-WOU75uIhq7YVrF0bH6LMrhNGR2YgZbK3pY=')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <h1 class="h1"> Welcome in Ebook</h1>
+        </div>
+    );
+}
+
+export default Home;
